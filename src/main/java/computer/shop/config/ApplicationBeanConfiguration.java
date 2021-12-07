f9235bbe4ac1d76faf91bfb1937f81cc6c846726
Vladikata2535 <kitaratapi4@abv.bg>
2021-12-07 15:50:49 +0200

    Implemented successfully internationalization in the App.

--- src/main/java/computer/shop/config/ApplicationBeanConfiguration.java
+++ src/main/java/computer/shop/config/ApplicationBeanConfiguration.java
@@ -1,10 +1,15 @@
 package computer.shop.config;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
 public class ApplicationBeanConfiguration {
@@ -17,6 +22,28 @@ public class ApplicationBeanConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new Pbkdf2PasswordEncoder();
+    }
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
+        cookieLocaleResolver.setCookieName("lang");
+        return cookieLocaleResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");
+        return interceptor;
+    }
+
+    @Bean
+    public MessageSource messageSource(){
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("i18n/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
     }
 }
 
--- src/main/java/computer/shop/config/WebConfiguration.java
+++ src/main/java/computer/shop/config/WebConfiguration.java
@@ -4,18 +4,22 @@ import computer.shop.web.interceptor.StatsInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
     private final StatsInterceptor statsInterceptor;
+    private final LocaleChangeInterceptor localeChangeInterceptor;
 
-    public WebConfiguration(StatsInterceptor statsInterceptor) {
+    public WebConfiguration(StatsInterceptor statsInterceptor, LocaleChangeInterceptor localeChangeInterceptor) {
         this.statsInterceptor = statsInterceptor;
+        this.localeChangeInterceptor = localeChangeInterceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(statsInterceptor);
+        registry.addInterceptor(localeChangeInterceptor);
     }
 }
